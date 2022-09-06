8ff1ad310f55ebb7e33abc7c149d50ae5bde143a
leonandhard <ruoxi.bao96@gmail.com>
2022-09-06 14:10:32 +1000

    Iinit

---
+++ blog/src/test/java/com/leonhard/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonhard.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
